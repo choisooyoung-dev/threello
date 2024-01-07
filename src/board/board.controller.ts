e98f871164fc0a9891bf00f26d25adfa5dfddddf
sangkwonlee1722 <kjess8335@gmail.com>
2024-01-08 04:45:34 +0900

    feat:보드 생성, 조회
    새 보드 생성
    전체 보드 목록 조회
    ID를 기반으로 특정 보드 조회

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,34 +1,27 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { BoardService } from './board.service';
 import { CreateBoardDto } from './dto/create-board.dto';
-import { UpdateBoardDto } from './dto/update-board.dto';
+import { Board } from './entities/board.entity';
 
-@Controller('board')
+@Controller('boards')
 export class BoardController {
-  constructor(private readonly boardService: BoardService) {}
+  constructor(private boardService: BoardService) {}
 
+  // 새 보드 생성
   @Post()
-  create(@Body() createBoardDto: CreateBoardDto) {
-    return this.boardService.create(createBoardDto);
+  async createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
+    return await this.boardService.createBoard(createBoardDto);
   }
 
+  // 전체 보드 목록 조회
   @Get()
-  findAll() {
-    return this.boardService.findAll();
+  async getAllBoards(): Promise<Board[]> {
+    return await this.boardService.getAllBoards();
   }
 
+  // ID를 기반으로 특정 보드 조회
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.boardService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBoardDto: UpdateBoardDto) {
-    return this.boardService.update(+id, updateBoardDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.boardService.remove(+id);
+  async getBoardById(@Param('id') id: number): Promise<Board> {
+    return await this.boardService.getBoardById(id);
   }
 }

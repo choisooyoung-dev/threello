91580c5ab3ebc5e5960286e47b09d77f35bc3830
smy1308 <smy1308@gmail.com>
2024-01-07 23:23:18 +0900

    feat - comment.entitiy 작성

--- src/comment/dto/create-comment.dto.ts
+++ src/comment/dto/create-comment.dto.ts
@@ -1,1 +1,12 @@
-export class CreateCommentDto {}
+import { PickType } from '@nestjs/mapped-types';
+import { Comment } from '../entities/comment.entity';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateCommentDto extends PickType(Comment, ['content']) {
+  /**
+   * 댓글 작성
+   */
+  @IsNotEmpty({ message: '내용을 입력해 주세요.' })
+  @IsString()
+  content: string;
+}
--- src/comment/entities/comment.entity.ts
+++ src/comment/entities/comment.entity.ts
@@ -1,1 +1,41 @@
-export class Comment {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import { Card } from 'src/card/entities/card.entity';
+import { User } from 'src/user/entities/user.entity';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+@Entity({ name: 'comments' })
+export class Comment {
+  @PrimaryColumn({ unsigned: true })
+  id: number;
+
+  @Column({ unsigned: true })
+  cardId: number;
+
+  @Column({ unsigned: true })
+  userId: number;
+
+  @IsNotEmpty({ message: '내용을 입력해 주세요.' })
+  @IsString()
+  @Column()
+  content: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @ManyToOne(() => User, (user) => user.comments, { onDelete: 'CASCADE' })
+  user: User;
+
+  @ManyToOne(() => Card, (card) => card.comments, { onDelete: 'CASCADE' })
+  card: Card;
+}

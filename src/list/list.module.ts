56dde6815c47cd2463bb0d56b52dbe6f55f76950
visitor17564 <lanlanloo17564@gmail.com>
2024-01-08 14:47:40 +0900

    fix - creat함수 버그수정

--- src/list/list.module.ts
+++ src/list/list.module.ts
@@ -3,10 +3,12 @@ import { ListService } from './list.service';
 import { ListController } from './list.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { List } from './entities/list.entity';
+import { BoardService } from '../board/board.service';
+import { Board } from '../board/entities/board.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([List])],
-  providers: [ListService],
+  imports: [TypeOrmModule.forFeature([List, Board])],
+  providers: [ListService, BoardService],
   controllers: [ListController],
 })
 export class ListModule {}

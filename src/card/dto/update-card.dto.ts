228d72793c81f84d7f2db209ab95a8b9589455f1
sooyoung choi <sooyoung.choi.dev@gmail.com>
2024-01-08 11:56:44 +0900

    Feat card (#9)
    * Fix - 카드 생성 dto 수정
    
    * Update - 작업자 할당
    
    * Update - 작업자 삭제, 카드 수정, 삭제
    
    * Update - 카드 인덱스 컬러 enum 변경, enum 타입 파일 생성, 타입 변경 후 관련 dto 파일들 수정

--- src/card/dto/update-card.dto.ts
+++ src/card/dto/update-card.dto.ts
@@ -1,4 +1,16 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCardDto } from './create-card.dto';
+import { IsOptional, IsString } from 'class-validator';
+import { Column } from 'typeorm';
+import { cardIndexColor } from '../types/card.color.type';
 
-export class UpdateCardDto extends PartialType(CreateCardDto) {}
+export class UpdateCardDto extends PartialType(CreateCardDto) {
+  @IsString()
+  title?: string;
+  @IsOptional()
+  @IsString()
+  @Column({ type: 'enum', enum: cardIndexColor, nullable: true })
+  color?: cardIndexColor;
+  @IsString()
+  content?: string;
+}

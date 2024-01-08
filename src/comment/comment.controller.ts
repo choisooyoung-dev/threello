3354ef01c94e51020f83830ce2a09931a5d0778d
smy1308 <smy1308@gmail.com>
2024-01-08 11:40:51 +0900

    Fix - 코멘트 주석 수정

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  HttpStatus,
 } from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
@@ -15,9 +16,15 @@ import { UpdateCommentDto } from './dto/update-comment.dto';
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
 
+  // 댓글 생성
   @Post()
-  create(@Body() createCommentDto: CreateCommentDto) {
-    return this.commentService.create(createCommentDto);
+  async create(@Body() createCommentDto: CreateCommentDto) {
+    const data = await this.commentService.create(createCommentDto);
+    return {
+      statusCode: HttpStatus.CREATED,
+      message: '댓글 작성 완료.',
+      data,
+    };
   }
 
   @Get()
@@ -30,11 +37,13 @@ export class CommentController {
     return this.commentService.findOne(+id);
   }
 
+  // 댓글 수정
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCommentDto: UpdateCommentDto) {
     return this.commentService.update(+id, updateCommentDto);
   }
 
+  // 댓글 삭제
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.commentService.remove(+id);
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,25 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Comment } from './entities/comment.entity';
 
 @Injectable()
 export class CommentService {
-  create(createCommentDto: CreateCommentDto) {
-    return 'This action adds a new comment';
+  constructor(
+    @InjectRepository(Comment) private commentRepository: Repository<Comment>,
+  ) {}
+
+  // 댓글 생성
+  async create(createCommentDto: CreateCommentDto) {
+    const { content } = createCommentDto;
+    const newComment = await this.commentRepository.save({ content });
+    return newComment;
   }
 
-  findAll() {
-    return `This action returns all comment`;
+  // 댓글 조회
+  async findAll() {
+    const findAllComments = await this.commentRepository.find();
+    return findAllComments;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} comment`;
+  // 특정 댓글 조회
+  async findOne(id: number) {
+    const findOndComment = await this.commentRepository.findOneBy({ id });
+    return findOndComment;
   }
 
+  // 댓글 수정
   update(id: number, updateCommentDto: UpdateCommentDto) {
     return `This action updates a #${id} comment`;
   }
 
+  // 댓글 삭제
   remove(id: number) {
     return `This action removes a #${id} comment`;
   }

210c37a0c426f24dc21f5184060669c7296d3d06
SangkwonLee <147799382+sangkwonlee1722@users.noreply.github.com>
2024-01-08 11:41:17 +0900

    Feat:board crud 작성 (#6)
    * feat:보드 엔티티 작성
    
    * feat:보드 생성, 조회
    
    새 보드 생성
    전체 보드 목록 조회
    ID를 기반으로 특정 보드 조회
    
    * feat:보드 수정, 삭제

--- src/board/entities/board.entity.ts
+++ src/board/entities/board.entity.ts
@@ -1,1 +1,30 @@
-export class Board {}
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('boards')
+export class Board {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @IsNotEmpty({ message: '보드명을 입력해 주세요.' })
+  @IsString()
+  @Column()
+  title: string;
+
+  @IsOptional()
+  @IsString()
+  @Column({ nullable: true })
+  color?: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}

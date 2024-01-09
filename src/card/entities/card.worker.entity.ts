59de860f36c24885183a1a1b15c6e4c5ff989f69
sooyoung.choi <sooyoung.choi.dev@gmail.com>
2024-01-09 21:07:16 +0900

    Fix - 코드 복구

--- src/card/entities/card.worker.entity.ts
+++ src/card/entities/card.worker.entity.ts
@@ -1,10 +1,4 @@
-import {
-  Column,
-  Entity,
-  JoinColumn,
-  ManyToOne,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Card } from './card.entity';
 import { IsNumber } from 'class-validator';
 import { User } from 'src/user/entities/user.entity';
@@ -18,20 +12,15 @@ export class CardWorker {
   @Column()
   user_id: number;
 
-  @ManyToOne(() => Card, (card) => card.cardWorkers)
-  card: Card;
-
   @ManyToOne(() => User, (user) => user.cardWorkers, {
     onDelete: 'CASCADE',
     onUpdate: 'CASCADE',
   })
-  @JoinColumn([{ name: 'cardWorker_id', referencedColumnName: 'id' }])
-  User: User;
+  user: User;
 
   @ManyToOne(() => Card, (card) => card.cardWorkers, {
     onDelete: 'CASCADE',
     onUpdate: 'CASCADE',
   })
-  @JoinColumn([{ name: 'cardWorker_id', referencedColumnName: 'id' }])
-  Card: Card;
+  card: Card;
 }

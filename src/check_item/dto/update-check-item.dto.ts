97164c1beb95aa208f8bab70f3c3f340789ee4bd
wlals7565 <117993640+wlals7565@users.noreply.github.com>
2024-01-11 00:13:05 +0900

    DOCS - swagger api docs

--- src/check_item/dto/update-check-item.dto.ts
+++ src/check_item/dto/update-check-item.dto.ts
@@ -1,13 +1,15 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, IsBoolean } from 'class-validator';
 import { CreateCheckItemDto } from './create-check-item.dto';
 
 export class UpdateCheckItemDto extends PartialType(CreateCheckItemDto) {
   @IsString()
   @IsNotEmpty({ message: '컨텐츠를 입력해주세요.' })
+  @ApiProperty({ description: '체크 아이템 내용', example: 'checkItemContent' })
   content: string;
 
   @IsBoolean()
   @IsNotEmpty({ message: '상태를 입력해주세요' })
+  @ApiProperty({ description: `작업 완료 여부`, example: true })
   is_done: boolean;
 }

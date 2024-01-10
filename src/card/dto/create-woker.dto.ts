4cf99b31fc277170e9dce5dcee87a7261bff2176
sooyoung.choi <sooyoung.choi.dev@gmail.com>
2024-01-11 06:03:56 +0900

    Update - 작업자 할당 스웨거 추가

--- src/card/dto/create-woker.dto.ts
+++ src/card/dto/create-woker.dto.ts
@@ -1,6 +1,12 @@
-import { IsArray } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, IsNotEmpty } from 'class-validator';
 
 export class CreateWorkerDto {
+  @ApiProperty({
+    description: '작업자 할당입니다.',
+    example: '[{"id":1},{"id":2}]',
+  })
+  @IsNotEmpty()
   @IsArray()
   userIds: { id: number }[];
 }

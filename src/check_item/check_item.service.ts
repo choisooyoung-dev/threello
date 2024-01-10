d8808cb11f37f0d3de81a61da3ded6c5460c91d2
visitor17564 <lanlanloo17564@gmail.com>
2024-01-11 04:22:04 +0900

    fix - 순서조정관련 완벽 수정

--- src/check_item/check_item.service.ts
+++ src/check_item/check_item.service.ts
@@ -24,7 +24,6 @@ export class CheckItemService {
     const checkItems = await this.checkItemRepository
       .createQueryBuilder('check_item')
       .where('check_item.checklist_id = :checklist_id', { checklist_id })
-      .leftJoinAndSelect('check_item.check_item', 'check_item')
       .getRawMany();
 
     return checkItems;
@@ -46,6 +45,7 @@ export class CheckItemService {
     try {
       // 선택된 리스트블록 가져오기
       const itemBlock = await this.verifyListById(id);
+      const checklist_id = itemBlock.checkItem[0].checklist_id;
       // 리스트블록의 현재 order값과 옮기려는 값을 비교해서 최대값, 최소값 설정
       let max = 0;
       let min = 0;
@@ -58,14 +58,17 @@ export class CheckItemService {
       }
       // 최대값과 최소값 사이에 있는 order를 가진 모든 리스트 불러오기
       const currentCheckItems = await this.checkItemRepository
-        .createQueryBuilder('checklist')
+        .createQueryBuilder('check_item')
         .where(
-          'checklist.lists_order >= :min AND checklist.lists_order <= :max',
+          'check_item.lists_order >= :min AND check_item.lists_order <= :max',
           {
             min: min,
             max: max,
           },
         )
+        .andWhere('check_item.checklist_id = :checklist_id', {
+          checklist_id: checklist_id,
+        })
         .getMany();
       // 뒤에서 앞으로 옮기면 앞에있던 모든리스트를 +1, 반대는 -1해야함
       // direction으로 변수설정하고 적용
@@ -108,11 +111,11 @@ export class CheckItemService {
       const { content } = item;
       await this.remove(itemId);
       const CreateCheckItemDto = { checklist_id: listTo, content };
-      await this.create(CreateCheckItemDto, listCount + 1);
+      await this.create(CreateCheckItemDto, listCount);
       const newItem = await this.checkItemRepository.find({
         where: {
           checklist_id: listTo,
-          lists_order: listCount + 1,
+          lists_order: listCount,
         },
       });
       await this.moveCheckItemBlock(newItem[0].id, itemTo);
@@ -129,14 +132,15 @@ export class CheckItemService {
   // 삭제
   async remove(id: number) {
     // 일단 리스트를 열람하고 얘가 전체중 몇번째애인지 확인
-    const list = (await this.verifyListById(id)).checkItem[0];
-    const checklist_id = list.checklist_id;
+    const item = (await this.verifyListById(id)).checkItem[0];
+    const checklist_id = item.checklist_id;
     const count = await this.count(checklist_id);
+    console.log(count);
 
     // 얘가 맨 마지막 애라면 나머지 order는 변경할 필요 없이 지우고 끗
-    if (Number(count.total_list_count) === list.lists_order) {
+    if (Number(count.total_list_count) === item.lists_order) {
       await this.checkItemRepository.delete({ id });
-      return list; // 리턴해서 아래 코드 실행하지 않게 함
+      return item; // 리턴해서 아래 코드 실행하지 않게 함
     }
     // 아니라 중간에 껴 있는 애라면 뒤에있는애들 다 앞으로 1칸씩 당겨야함
     // 다시만들면 귀찮으니까 지울 애를 맨 뒤로 보낸 후, 삭제하는것으로 정리
@@ -145,10 +149,10 @@ export class CheckItemService {
   }
 
   // 지원메서드 count(전체 list 개수를 세 줌)
-  async count(boards_id: number) {
+  async count(checklist_id: number) {
     const listCount = await this.checkItemRepository
-      .createQueryBuilder('checklist')
-      .where({ boards_id: boards_id })
+      .createQueryBuilder('check_item')
+      .where({ checklist_id: checklist_id })
       .select('COUNT(check_item.lists_order)', 'total_list_count')
       .getRawOne();
 

134b17452111a04610a1f6745a8a737d6dd4e278
Min Young <146905861+smy1308@users.noreply.github.com>
2024-01-10 19:33:53 +0900

    Fix - board엔티티 병합충돌 해결 (#36)

--- src/board/entities/board.entity.ts
+++ src/board/entities/board.entity.ts
@@ -2,6 +2,7 @@ import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 import { List } from '../../list/entities/list.entity';
 import { BoardMember } from './board-member.entity';
+import { Comment } from 'src/comment/entities/comment.entity';
 import { Color } from 'src/common/types/color.type';
 
 @Entity('boards')
@@ -24,4 +25,7 @@ export class Board {
 
   @OneToMany(() => BoardMember, (boardMembers) => boardMembers.board) // 보드 멤버와의 일대다 관계 설정
   boardMembers: BoardMember[];
+
+  @OneToMany(() => Comment, (comments) => comments.board)
+  comments: Comment[];
 }
